ccf7dfc79ab456c2e22d0f9473daf7463fb1297d
fer <fernanda.1991.alejandra@gmail.com>
2020-08-06 15:33:46 -0400

    terminando tarjeta de contacto

--- data.js
+++ data.js
@@ -8,22 +8,17 @@ const click = document.getElementById("get");
         console.log(data.results['0'])
   
     card.innerHTML= `<div class="card" id="content">
-    <div class="row">
-    <div class="col s12 m2">
-      <div class="card">
-        <div class="card-image">
-          <img src="${data.results['0'].picture.large}">
-          <span class="card-title">${data.results['0'].name.title}</span>
-        </div>
-        <div class="card-content">
-          <p>Hello, my name is ${data.results['0'].name.first} ${data.results['0'].name.last}, I am ${data.results['0'].dob.age} years old and I am from ${data.results['0'].location.city}, ${data.results['0'].location.country}. My contact details are, e-mail ${data.results['0'].email}, and my cell phone is ${data.results['0'].phone}
-          </p>
-        </div>
-        
-      </div>
-    </div>
-  </div>
-</div>`
+                      <div class="card" style="width: 20rem; ">
+                       <img src="${data.results['0'].picture.large}"class="card-img-top">
+                        <span class="card-title">${data.results['0'].name.title}</span>
+                          <div class="card-body">
+                            <p>Hello, my name is ${data.results['0'].name.first} ${data.results['0'].name.last}, I am ${data.results['0'].dob.age} years old and I am from ${data.results['0'].location.city}, ${data.results['0'].location.country}. E-mail ${data.results['0'].email}, and my cell phone is ${data.results['0'].phone}
+                              </p>
+      
+                             </div>
+                        </div>
+                     </div>`
+
 
                 })
  });
